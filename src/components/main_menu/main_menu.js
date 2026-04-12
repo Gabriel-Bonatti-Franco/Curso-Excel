534964a9567dbfe52d174fd517b1be97717eceb1
Gabriel-Bonatti-Franco <gabrielbonatii12@gmail.com>
2026-04-12 19:17:49 -0300

    Adding Validating File Structure in ci.yml file, and Refactoring some .html files

--- src/components/main_menu/main_menu.js
+++ src/components/main_menu/main_menu.js
@@ -1,14 +1,14 @@
-class NavMenu extends HTMLElement {
+class MainMenu extends HTMLElement {
   async connectedCallback() {
     // shadow DOM isola o CSS do componente
     const shadow = this.attachShadow({ mode: 'open' });
 
     // Busca o HTML externo e injeta no Shadow DOM
-    const res = await fetch('/components/nav_menu.html');
+    const res = await fetch('/src/components/main_menu/main_menu.html');
     const html = await res.text();
 
     shadow.innerHTML = html;
   }
 }
 
-customElements.define('nav-menu', NavMenu);
+customElements.define('main-menu', MainMenu);
