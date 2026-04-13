da149e3beba15e36290a4cb5c547fb3a0ba339dc
Gabriel-Bonatti-Franco <gabrielbonatii12@gmail.com>
2026-04-13 18:38:35 -0300

    Adding a Card component

--- src/components/my_card/my_card.js
+++ src/components/my_card/my_card.js
@@ -1,15 +1,18 @@
-class MainMenu extends HTMLElement {
+class MyCard extends HTMLElement {
   async connectedCallback() {
     // shadow DOM isola o CSS do componente
     const shadow = this.attachShadow({ mode: 'open' });
-    console.log(this.textContent)
+    
+    const text = this.textContent
 
     // Busca o HTML externo e injeta no Shadow DOM
-    const res = await fetch('/src/components/main_menu/main_menu.html');
+    const res = await fetch('/src/components/my_card/my_card.html');
     const html = await res.text();
 
     shadow.innerHTML = html;
+    shadow.querySelector("#content").textContent = text;
+    
   }
 }
 
-customElements.define('main-menu', MainMenu);
+customElements.define('my-card', MyCard);
