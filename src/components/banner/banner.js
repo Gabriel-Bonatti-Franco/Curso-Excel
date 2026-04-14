e66e5d67593f9318c482d2141645e27ab11ca8d2
Gabriel-Bonatti-Franco <gabrielbonatii12@gmail.com>
2026-04-13 22:01:52 -0300

    Adding Banner and Carrossel Components

---
+++ src/components/banner/banner.js
@@ -0,0 +1,18 @@
+class MainBanner extends HTMLElement {
+  async connectedCallback() {
+    // shadow DOM isola o CSS do componente
+    const shadow = this.attachShadow({ mode: 'open' });
+
+    const text_content = this.getHTML()
+    console.log(text_content)
+
+    // Busca o HTML externo e injeta no Shadow DOM
+    const res = await fetch('/src/components/banner/banner.html');
+    const html = await res.text();
+
+    shadow.innerHTML = html;
+    shadow.querySelector('#content').innerHTML = text_content
+  }
+}
+
+customElements.define('main-banner', MainBanner);
