98b436b4234ccf791da3374f64dbf3339ae9fde3
Gabriel-Bonatti-Franco <gabrielbonatii12@gmail.com>
2026-04-26 20:31:42 -0300

    Adding Testes with Jest

---
+++ git_commit_push.bat
@@ -0,0 +1,31 @@
+@echo off
+
+if "%~1"=="" (
+    echo Error: please provide a commit message
+    exit /b 1
+) else (
+    echo Your commit will have the message %1 
+    echo.
+    choice /c YN /m "Do you want to continue?"
+
+    if %errorlevel%==2 (
+        echo Cancelled by user
+        echo Done
+        exit /b 1
+    )
+
+    if %errorlevel%==1 (
+        git add .
+        echo.
+        echo Files added to staged changes
+        echo.
+
+        git status --short
+        git commit -m %1
+        git push
+
+        echo Commit complete
+        echo Done.
+        exit /b 0
+    )
+)
